0543d4bae4b383cf239731bef0fa8f98dfddaaa8
rafapereirabr <rafa.pereira.br@gmail.com>
2021-08-01 21:56:41 -0300

    select random tract

--- R/script.R
+++ R/script.R
@@ -1,16 +1,14 @@
 library(sf)
 library(geobr)
 library(ggplot2)
+library(data.table)
 library(gdalio) ## remotes::install_github("hypertidy/gdalio")
 
 library(twitteR)
 library(rtweet)
 
-###### 1. Download census tract data -------------------------
-ct <- read_census_tract(code_tract = 'all', simplified = FALSE, year=2010)
-ct <- sf::st_make_valid(ct)
-ct$area <- st_area(ct)
-head(ct)
+###### 1. Download census tract sf data -------------------------
+ct <- read_census_tract(code_tract = 'all', simplified = TRUE, year=2010)
 
 ct_largetst <- subset(ct, area == max(ct$area)) # 150530405000051
 ct_smallest <- subset(ct, area == min(ct$area)) # 210060005000047 
@@ -21,8 +19,13 @@ ct_smallest <- subset(ct, area == min(ct$area)) # 210060005000047
 
 
 # select a census tract
-ct1 <- ct[1000,]
-ct1 <- ct_largetst
+all_tracts <- data.table::fread( './input/code_tract_all.csv')
+temp_code_tract <- sample(all_tracts$code_tract, 1, replace = FALSE, prob = NULL)
+ct1 <- subset(ct, code_tract == temp_code_tract) 
+# ct1 <- subset(ct, code_tract == 330340105000173) # 210060005000047 
+
+
+
 
 # get extent and projection
 projection <- sf::st_crs(ct1)$wkt
@@ -47,9 +50,12 @@ gdalio::gdalio_set_default_grid(list(extent = extent,
 #' and then throw our long lats and our raster source at it
 myraster <- gdalio_graphics(tfile, resample = "cubic")
 
+
+
+
 # crop and mask
 # my_crop <- raster::crop(myraster, vect(ct1))
-# my_mask <- terra::mask(my_crop, vect(ct1))
+# my_mask <- mask(my_crop, vect(ct1))
 
 
 ###### 4. Generate plot --------------------------------
@@ -64,7 +70,7 @@ temp_plot <- ggplot() +
               theme_void()
 
 
-ggsave(temp_plot, filename = './plot/temp_tract2.png', dpi=200)
+ggsave(temp_plot, filename = './plot/temp_tract10.png', dpi=200)
 
 
 
@@ -78,22 +84,34 @@ ggsave(temp_plot, filename = './plot/temp_tract2.png', dpi=200)
   coords <- st_coordinates(centroid)
   googlemaps_link <- paste0('https://www.google.com/maps/search/?api=1&query=',coords[2],',',coords[1])
 
+  
   # prepare tweet text
   name_muni <- ct1$name_muni
   code_tract <- ct1$code_tract
   zone <- tolower(ct1$zone)
   
-  # Tweet
+  # Create Tweet post 
   tweet_text <- paste0('Municipio de ', name_muni, '. Setor censitário n. ', code_tract, '. Zona ', zone, '. Link Google maps: ', googlemaps_link) 
 
-  
+  # authenticate
   token <- rtweet::get_tokens()
   
-# post Tweet
-# update blog post
-
-
+  # Tweet !
   post_tweet( status = tweet_text,
               media = './plot/temp_tract2.png')
   
+  # update blog post
+  
+  
+  # github actions
+  https://github.com/marketplace/actions/twitter-bot-action
+  https://itnext.io/tweet-from-github-actions-e289de58988a
+  
+  
+  https://www.rostrum.blog/2020/09/21/londonmapbot/
+  https://twitter.com/londonmapbot
+  
+  
+  https://enrico.spinielli.net/post/writing-a-twitter-bot-in-r/
+    https://twitter.com/italiancomuni
   
